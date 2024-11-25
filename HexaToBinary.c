9cd6e9717aa2e6ec2eae6d32b4f226e09b8633dc
Jason <141192101+jyod27@users.noreply.github.com>
2024-11-25 11:49:08 -0500

    Allows for continuous input unless 'x' is entered

--- HexaToBinary.c
+++ HexaToBinary.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main() {
+
+    while (1) {
+        char input;
+
+        printf("Enter a hexadecimal digit: (or 'X' to quit)\n");
+        scanf(" %c", &input);
+        
+        input = toupper(input);
+
+        if(input == 'X') {
+            break;
+        }
+
+        printf("%c\n", input);
+    }
     return 0;
 }
