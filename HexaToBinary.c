08061cd88bc288f742e8e18725f7af146e47e8f6
Jason <141192101+jyod27@users.noreply.github.com>
2024-11-25 12:11:52 -0500

    Adds function to check if input is valid

--- HexaToBinary.c
+++ HexaToBinary.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <ctype.h>
 
+int checkInput(char c) {
+    if (('0' <= c && c <= '9') || ('A' <= c && c <= 'F')) {
+        return 1;
+    } else if (c == 'X') {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
+int hexToDeciaml(char c) {
+
+}
+
+
 int main() {
 
     while (1) {
         char input;
 
-        printf("Enter a hexadecimal digit: (or 'X' to quit)\n");
+        printf("Enter a single hexadecimal digit: (or 'X' to quit)\n");
         scanf(" %c", &input);
-        
+
         input = toupper(input);
+        
+        int validInput = checkInput(input);
 
-        if(input == 'X') {
+        if (validInput == 2) {
             break;
+        } else if (validInput == 3) {
+            printf("Invalid entry, try again.");
+            continue;
+        } else {
+            printf("%c\n", input);
         }
-
-        printf("%c\n", input);
     }
     return 0;
 }
